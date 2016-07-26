7ea4b62fe24d2bc97fde851e06e8a3be2223cb95
pansay <pansay@users.noreply.github.com>
2016-07-26 21:30:01 +0200

    Create read_selection_in_pt_PT.js

---
+++ read_selection_in_pt_PT.js
@@ -0,0 +1,15 @@
+(function(synth, lang, langIndex) {
+
+    var voice = synth.getVoices().filter(voice => voice.lang === lang)[langIndex],
+        text = window.getSelection().toString();
+
+    function speak(text, voice) {
+        var utterance = new SpeechSynthesisUtterance(text);
+        utterance.voice = voice;
+        utterance.rate = .4;
+        synth.speak(utterance);
+    }
+
+    speak(text, voice);
+
+})(window.speechSynthesis, 'pt-PT', 0);
