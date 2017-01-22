70705c74252b49fa7cf1c31e60c4543ecf36700d
pansay <pansay@users.noreply.github.com>
2017-01-22 09:35:19 +0100

    Create duolingo_clear.js

---
+++ duolingo_clear.js
@@ -0,0 +1,1 @@
+javascript:$('.strength-5').closest('span>a').remove();$('.btn-lg.disabled').closest('.skill-tree-row').remove();$('a[href="/show_store$scroll=bonus"]').remove();$('.fluency-container').closest('.box-colored').remove();
