a172f9753488c0cdb4c0161f75ce1c45ed3be877
pansay <jan.krepelka@gmail.com>
2017-01-31 00:26:27 +0100

    typo fix

--- mediawiki_format_fr.js
+++ mediawiki_format_fr.js
@@ -3,7 +3,7 @@ var box = document.forms.editform.elements.wpTextbox1,
         ["([^'])'([^'])", '$1’$2'],             // apostrophe for single '
         [' {2,}', ' '],                         // remove non-single spaces
         ['"(.*?)"', '«$1»'],                    // French quotes from regular double quotes
-        ['« (.*?) »"', '«$1»'],                 // French quotes remove breaking spaces
+        ['« (.*?) »', '«$1»'],                 // French quotes remove breaking spaces
         ['«&nbsp;(.*?)&nbsp;»', '«$1»'],        // TODO improve this with better regexp below
         ['«(.*?)»', '«&nbsp;$1&nbsp;»'],        // French quotes add non-breaking spaces
         ['--', '-'],                            // French emdash
