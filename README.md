380e7514a17cbfd528f68188b87e25e1c80b4c13
pansay <jan.krepelka@gmail.com>
2017-02-04 11:22:00 +0100

    urgh negative lookbehind regexp in js

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # tts_bookmarklets
-Bookmarklets for tts (text-to-speech).
+
+Bookmarklets for tts (text-to-speech), duolingo, clozemaster, languages, typography, etc.
 
 
-Just copy the js source into a [bookmarklet maker](http://mrcoles.com/bookmarklet/) and enjoy!
+Just copy the js source into a [bookmarklet maker](http://bookmarklets.org/maker/) and enjoy!
 
 
--- mediawiki_format_fr.js
+++ mediawiki_format_fr.js
@@ -1,17 +1,22 @@
 var box = document.forms.editform.elements.wpTextbox1,
     replaces = [
-        ["([^'])'([^'])", '$1’$2'],             // apostrophe for single '
-        [' {2,}', ' '],                         // remove non-single spaces
-        ['"(.*?)"', '«$1»'],                    // French quotes from regular double quotes
-        ['« (.*?) »', '«$1»'],                  // French quotes remove breaking spaces
-        ['«&nbsp;(.*?)&nbsp;»', '«$1»'],        // TODO improve this with better regexp below
-        ['«(.*?)»', '«&nbsp;$1&nbsp;»'],        // French quotes add non-breaking spaces
-        ['--', '-'],                            // French emdash
-        [' ([;|?|!])', '&#8239;$1'],            // French replace breaking with non-breaking thin space before ;?!
-        [' :', '&nbsp;:'],                      // French replace breaking with non-breaking space before :
+        ["([^'])'([^'])", '$1’$2'],                     // apostrophe for single '
+        [' {2,}', ' '],                                 // remove non-single spaces
+        ['"(.*?)"', '«$1»'],                            // French quotes from regular double quotes
+        ['« (.*?) »', '«$1»'],                          // French quotes remove breaking spaces
+        ['« (.*?) »', '«$1»'],                          // French quotes remove non-breaking spaces
+        ['«&nbsp;(.*?)&nbsp;»', '«$1»'],                // French quotes remove non-breaking spaces TODO improve this with better regexp below
+        ['«(.*?)»', '«&nbsp;$1&nbsp;»'],                // French quotes add non-breaking spaces
+        ['--', '—'],                                    // French emdash
+        ['— ', '—'],                                    // French emdash remove non-breaking spaces
+        [' —', ' —'],                                   // French emdash remove non-breaking spaces
+        ['— ([^.]*?) —', '—&nbsp;$1&nbsp;—'],           // French emdash closed add non-breaking spaces
+        // grr won't work in js ['(?<!&nbsp;)— ', '—&nbsp;'],           // French emdash opened add non-breaking space
+        ['((?!&nbsp;).{6}|^.{0,5})— ', '$1—&nbsp;'],    // French emdash opened add non-breaking space
+        [' ([;|?|!])', '&#8239;$1'],                    // French replace breaking with non-breaking thin space before ;?!
+        [' :', '&nbsp;:']                               // French replace breaking with non-breaking space before :
     ],
     replace;
-
 
 while ( replace = replaces.shift() ) {
     box.value=box.value.replace(new RegExp(replace[0],'ig'),replace[1]);
