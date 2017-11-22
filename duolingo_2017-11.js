f81222801c1fa8af55a736c30dbacd1116586c84
pansay <pansay@users.noreply.github.com>
2017-11-22 06:31:30 +0100

    Update duolingo_2017-11.js

--- duolingo_2017-11.js
+++ duolingo_2017-11.js
@@ -1,5 +1,5 @@
 // remove test out buttons
-document.querySelectorAll('._2GJb6>button, a[data-test=test-out-button]').forEach(item => item.parentNode.removeChild(item));
+document.querySelectorAll('._2GJb6>button,._1H-7I, a[data-test=test-out-button]').forEach(item => item.parentNode.removeChild(item));
 
 // remove undone
 document.querySelectorAll('.W1dac>span:first-child').forEach(item => {if(item.parentNode.nodeName === 'SPAN'){item.parentNode.parentNode.removeChild(item.parentNode)}});
