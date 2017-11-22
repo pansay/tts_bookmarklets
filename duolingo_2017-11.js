65f8133d82acb92bebc0be6f52d5b1903c3c99f7
pansay <pansay@users.noreply.github.com>
2017-11-22 06:30:04 +0100

    Update duolingo_2017-11.js

--- duolingo_2017-11.js
+++ duolingo_2017-11.js
@@ -1,1 +1,8 @@
-document.querySelectorAll('._2GJb6>button, a[data-test=test-out-button]').forEach(item => item.parentNode.removeChild(item));document.querySelectorAll('.W1dac>span:first-child').forEach(item => {if((item.parentNode.nodeName === 'SPAN' || (item.querySelectorAll('span').length === 2))){item.parentNode.parentNode.removeChild(item.parentNode)}});document.querySelectorAll('._1H-7I._2GJb6').forEach(item => item.parentNode.removeChild(item));
+// remove test out buttons
+document.querySelectorAll('._2GJb6>button, a[data-test=test-out-button]').forEach(item => item.parentNode.removeChild(item));
+
+// remove undone
+document.querySelectorAll('.W1dac>span:first-child').forEach(item => {if(item.parentNode.nodeName === 'SPAN'){item.parentNode.parentNode.removeChild(item.parentNode)}});
+
+// remove done but faded
+document.querySelectorAll('.W1dac').forEach(item => {if(!item.querySelectorAll('._2TMjc').length){item.parentNode.removeChild(item)}});
