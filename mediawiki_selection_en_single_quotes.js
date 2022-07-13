c9ca72210d1d5a2c082ef1911ebdfeb0dd8136d9
pansay <pansay@users.noreply.github.com>
2022-07-13 09:15:32 +0200

    Create mediawiki_selection_en_single_quotes.js

---
+++ mediawiki_selection_en_single_quotes.js
@@ -0,0 +1,9 @@
+var box = document.forms.editform.elements.wpTextbox1,
+    len = box.value.length,
+    start = box.selectionStart,
+    end = box.selectionEnd,
+    selectedText = box.value.substring(start, end),
+    txtBefore = box.value.substring(0, start),
+    txtAfter = box.value.substring(end, len);
+
+box.value = txtBefore + '‘' + selectedText + '’' + txtAfter;
