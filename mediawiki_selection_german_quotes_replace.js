897148c90761a414e74db95e960ec11efe702a6c
pansay <pansay@users.noreply.github.com>
2022-07-16 10:55:56 +0200

    Create mediawiki_selection_german_quotes_replace.js

---
+++ mediawiki_selection_german_quotes_replace.js
@@ -0,0 +1,10 @@
+var box = document.forms.editform.elements.wpTextbox1,
+    len = box.value.length,
+    start = box.selectionStart,
+    end = box.selectionEnd,
+    selectedText = box.value.substring(start, end),
+    txtBefore = box.value.substring(0, start),
+    txtAfter = box.value.substring(end, len);
+
+selectedText = selectedText.replace(new RegExp('"(.*?)"', 'ig'), '‘„$1“');
+box.value = txtBefore + selectedText + txtAfter;
